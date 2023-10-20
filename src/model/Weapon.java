9922b81442e5d22c40ddd732e352cb41a66a6a58
loftykonuk <alperenkonuk93@gmail.com>
2023-10-20 22:07:09 +0300

    Weapon-enum

---
+++ src/model/Weapon.java
@@ -0,0 +1,22 @@
+package model;
+
+public enum Weapon {
+    MELEE,
+    CLASSIC,
+    SHORTY,
+    FRENZY,
+    GHOST,
+    SHERIFF,
+    STINGER,
+    SPECTRE,
+    BUCKY,
+    JUDGE,
+    BULLDOG,
+    GUARDIAN,
+    PHANTOM,
+    VANDAL,
+    MARSHAL,
+    OPERATOR,
+    ARES,
+    ODIN,
+}
